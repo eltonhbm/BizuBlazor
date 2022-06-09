13a49bd2354128a4fd693f48fc3d72e610cd70e5
Elton Monteiro <elton@sistemasbr.net>
2022-06-09 20:57:53 -0300

    Add project files.

---
+++ BizuBlazor/Server/Features/Cep/Controllers/CepController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BizuBlazor.Server.Features.Cep.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CepController : ControllerBase
+    {
+        
+    }
+}
---
+++ BizuBlazor/Shared/Features/Cep/Models/Endereco.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizuBlazor.Shared.Features.Cep.Models
+{
+    internal class Endereco
+    {
+        public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string localidade { get; set; }
+        public string uf { get; set; }
+        public string ibge { get; set; }
+        public string ddd { get; set; }
+        public bool erro { get; set; }
+    }
+}
